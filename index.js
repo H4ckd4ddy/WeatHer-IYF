f7ad267fe2ba2ae9619ce123110c04f1296327e2
Etienne SELLAN <git@sellan.fr>
2018-11-15 15:08:39 +0100

    Add meteo object

--- index.js
+++ index.js
@@ -4,17 +4,23 @@ const fs = require('fs')
 const sqlite3 = require('sqlite3')
 
 const test = require('./test')
+const meteo = require('./meteo')
 
 // SETTINGS
-const databaseFile = "WeatHer-IYF.db"
+const databaseFile = 'WeatHer-IYF.db'
 // END SETTINGS
 
 programm
-    .version("1.0.0")
+    .version('1.0.0')
     .option('-t, --test', 'exemple')
+    .option('-m, --meteo', 'Get Meteo Data')
 
 programm.parse(process.argv)
 
 if(programm.test){
     let exemple = new test()
+}else if(programm.meteo){
+    let meteo = new meteo()
+}else{
+    console.log('Help')
 }
---
+++ meteo.js
@@ -0,0 +1,14 @@
+class meteo {
+    
+    constructor(){
+        this.msg = "hello world"
+        this.boom()
+    }
+    
+    boom(){
+        console.log(this.msg)
+    }
+    
+}
+
+module.exports = meteo
