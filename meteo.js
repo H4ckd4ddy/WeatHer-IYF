2faaeacf3f4c179d97fbf22b4301cae218bb0294
MouleFrites <louisarmand.wilfart@ynov.com>
2018-11-15 16:44:38 +0100

    meteo

--- meteo.js
+++ meteo.js
@@ -1,14 +1,26 @@
+const request = require('request');
 class meteo {
-    
+
     constructor(){
-        this.msg = "hello world"
-        this.boom()
+        this.weatherStart();
     }
-    
-    boom(){
-        console.log(this.msg)
+
+    weatherStart() {
+
+        let apiKey = 'c3ba801c7ef6fc89d70a04ee6c75d80d';
+        let city = 'bordeaux';
+        let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+        request(url, function (err, response, body) {
+            if (err) {
+                console.log('error:', error);
+            } else {
+                console.log('body:', body);
+                let weather = JSON.parse(body);
+                console.log(weather.name)
+            }
+        });
     }
-    
 }
 
 module.exports = meteo
